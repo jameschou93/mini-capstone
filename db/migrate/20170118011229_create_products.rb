a359a69fb14b65d1d0b1172025df151b9b352090
James Chou <jameschou1993@gmail.com>
2017-01-17 20:12:43 -0500

    corrected model

--- db/migrate/20170118011229_create_products.rb
+++ db/migrate/20170118011229_create_products.rb
@@ -1,6 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
+      t.string :game_name
+      t.string :player_min
+      t.string :player_max
+      t.string :time
+      t.string :age_limit
 
       t.timestamps
     end
