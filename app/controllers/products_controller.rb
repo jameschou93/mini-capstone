cac5503f58f4810db2d3883a8873941e55c5b576
James Chou <jameschou1993@gmail.com>
2017-01-19 20:48:50 -0500

    added new game form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,12 @@ class ProductsController < ApplicationController
     p @boardgames
     render 'products.html.erb'
   end
+
+  def newprod
+    
+  end
+
+  def result
+    @game = Product.create(game_name: params[:input_game_name], player_min: params[:input_player_min], player_max: params[:input_player_max], time: params[:input_time], age_limit: params[:input_age_limit], image: params[:input_image])
+  end
 end
